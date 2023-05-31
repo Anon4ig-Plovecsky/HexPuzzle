bef5afbf8df1e1ff779c49739db06848b132bc70
Anon4ig_Plovecsky <86836359+Anon4ig-Plovecsky@users.noreply.github.com>
2023-05-31 21:38:11 +0600

    Added cube spawn

--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -1,22 +1,44 @@
-using System;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.AddressableAssets;
+using Random = UnityEngine.Random;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using System;
 
 public class CubeGenerator : MonoBehaviour
 {
-    [SerializeField] private AssetLabelReference assetLabelReference; 
+    [SerializeField] private AssetLabelReference assetLabelReference;
     private readonly Tuple<int, int> cubeQty = new(3, 2); //[SerializeField]
+    private GameObject cubesParent;
+    //Cubes
+    private readonly List<Transform> pointsOfSpawn = new();
+    private readonly HashSet<int> cubeSpawn = new();
+    private GameObject[] cubesGameObjects;
+    private GameObject[] cubesPrefab;
+    private Shader cubeShader;
+    //Paintings
     private ImageCreator imageCreator;
     private List<Sprite> paintings;
     private Sprite mainPainting;
     private void Start()
     {
-        var asyncOperationHandle = Addressables.LoadAssetsAsync<Sprite>(assetLabelReference, _ => {});
-        asyncOperationHandle.Completed += delegate { OnLoadDone(asyncOperationHandle); };
+        cubesParent = GameObject.Find("Cubes");
+        cubeShader = Shader.Find("Standard");
+        cubesPrefab = new GameObject[cubeQty.Item1 * cubeQty.Item2];
+        cubesGameObjects = new GameObject[cubesPrefab.Length];
+        var asyncOperationIListHandle = Addressables.LoadAssetsAsync<Sprite>(assetLabelReference, _ => {});
+        asyncOperationIListHandle.Completed += delegate { OnLoadDone(asyncOperationIListHandle); };
+        var asyncOperationHandle = Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/PartOfPainting.prefab");
+        asyncOperationHandle.Completed += delegate
+        {
+            if(asyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
+                for (var i = 0; i < cubesPrefab.Length; i++)
+                    cubesPrefab[i] = asyncOperationHandle.Result;
+        };
+        var pointsOfSpawnParent = GameObject.Find("PointsOfSpawn");
+        for(var i = 0; i < pointsOfSpawnParent.transform.childCount; i++)
+            pointsOfSpawn.Add(pointsOfSpawnParent.transform.GetChild(i));
     }
     private void OnLoadDone(AsyncOperationHandle<IList<Sprite>> asyncOperationHandle)
     {
@@ -24,6 +46,7 @@ public class CubeGenerator : MonoBehaviour
         {
             paintings = new List<Sprite>(asyncOperationHandle.Result.ToArray());
             ChooseImage();
+            SpawnCubes();
         }
         else 
             Debug.Log("Failed to load paintings!");
@@ -32,5 +55,23 @@ public class CubeGenerator : MonoBehaviour
     {
         mainPainting = paintings[Random.Range(0, paintings.Count)];
         imageCreator = new ImageCreator(mainPainting, cubeQty);
+        imageCreator.CropPainting();
+    }
+    private void SpawnCubes()
+    {
+        while(cubeSpawn.Count < cubeQty.Item1 * cubeQty.Item2)
+            cubeSpawn.Add(Random.Range(0, pointsOfSpawn.Count));
+        for (var i = 0; i < cubesPrefab.Length; i++)
+        {
+            var material = new Material(cubeShader)
+            {
+                mainTexture = imageCreator.GetPartsOfPainting()[i].texture
+            };
+            cubesPrefab[i].transform.GetChild(0).GetComponent<Renderer>().material = material;
+            cubesGameObjects[i] = Instantiate(cubesPrefab[i], pointsOfSpawn[cubeSpawn.ToArray()[i]].position, 
+                pointsOfSpawn[cubeSpawn.ToArray()[i]].rotation);
+            cubesGameObjects[i].name = $"PartOfPainting ({i})";
+            cubesGameObjects[i].transform.SetParent(cubesParent.transform);
+        }
     }
 }
--- Assets/Scripts/ImageCreator.cs
+++ Assets/Scripts/ImageCreator.cs
@@ -1,12 +1,13 @@
-using System;
 using UnityEngine;
+using System;
 
 public class ImageCreator
 {
-    private Sprite painting;
-    private int height, width;
-    private int intervalHeight, intervalWidth;
+    private int intervalWidth;
+    private readonly Sprite painting;
+    private readonly int height, width;
     private readonly Tuple<int, int> cubeQty;
+    private int offsetByWidth, offsetByHeight;
     private readonly Sprite[] partsOfPaintings;
     public ImageCreator(Sprite mainPainting, Tuple<int, int> cubeQty)
     {
@@ -19,17 +20,28 @@ public class ImageCreator
     }
     private void CalculateWidth()
     {
-        
+        intervalWidth = Convert.ToInt32(Math.Min(height / cubeQty.Item2, width / cubeQty.Item1));
+        offsetByWidth = (width - intervalWidth * cubeQty.Item1) / 2;
+        offsetByHeight = (height - intervalWidth * cubeQty.Item2) / 2;
     }
-    public Sprite[] CropPainting()
+    public void CropPainting()
     {
-        for (var i = 0; i < cubeQty.Item1; i++)
+        for (var i = 0; i < cubeQty.Item2; i++)
         {
-            for (var j = 0; j < cubeQty.Item2; j++)
+            for (var j = 0; j < cubeQty.Item1; j++)
             {
-                // var sprite = Sprite.Create(painting.texture, new Rect())
+                var texture = new Texture2D(intervalWidth, intervalWidth);
+                texture.SetPixels(painting.texture.GetPixels(offsetByWidth + j * intervalWidth,
+                    offsetByHeight + i * intervalWidth, intervalWidth, intervalWidth));
+                texture.Apply();
+                var sprite = Sprite.Create(texture, new Rect(0f, 0f, intervalWidth, intervalWidth),
+                    new Vector2(0.5f, 0.5f));
+                partsOfPaintings[i * cubeQty.Item1 + j] = sprite;
             }
         }
+    }
+    public Sprite[] GetPartsOfPainting()
+    {
         return partsOfPaintings;
     }
 }
