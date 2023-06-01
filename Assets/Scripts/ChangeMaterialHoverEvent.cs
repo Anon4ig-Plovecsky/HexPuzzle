9098c0e83ae5ad83a4047ceca2ac1a6b55ea4ae9
Anon4ig_Plovecsky <86836359+Anon4ig-Plovecsky@users.noreply.github.com>
2023-06-02 01:19:31 +0600

    Added interactivity to cubes

---
+++ Assets/Scripts/ChangeMaterialHoverEvent.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class ChangeMaterialHoverEvent : MonoBehaviour
+{
+    private string nameOfObject;
+    private const int CubeSize = 6;
+    [SerializeField] private Material selectedMaterial;
+    private readonly Texture[] originalTextures = new Texture[CubeSize];
+    private readonly Material[] materialsOfObject = new Material[CubeSize];
+    private void Start()
+    {
+        nameOfObject = transform.parent.name;
+        for (var i = 0; i < CubeSize; i++)
+        {
+            materialsOfObject[i] = transform.parent.GetChild(0).GetChild(i).GetComponent<Renderer>().material;
+            originalTextures[i] = materialsOfObject[i].mainTexture;
+        }
+    }
+    public void ObjectSelected(bool selected)
+    {
+        for (var i = 0; i < CubeSize; i++)
+            materialsOfObject[i].mainTexture = selected ? selectedMaterial.mainTexture : originalTextures[i];
+    }
+}
