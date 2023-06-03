262f839837ae90de5dffcc7e15b4fb3885472567
Anon4ig_Plovecsky <86836359+Anon4ig-Plovecsky@users.noreply.github.com>
2023-06-04 00:27:42 +0600

    Added check correctness
    Added check for the correctness of the puzzle solution

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using System;
 
 public class GridController : GridConstructor
 {
+    private const string CorrectlyString = "[Correctly]";
+    private readonly Tuple<int, int> cubeQty = new(3, 2); //[SerializeField]
     private GameObject cubes;
+    private GameObject grid;
     protected override void Start()
     {
         base.Start();
         cubes = GameObject.Find("Cubes");
+        grid = GameObject.Find("Grid");
     }
-    void Update()
+    private void Update()
     {
-        
+        if (cubes.transform.childCount != cubeQty.Item1 * cubeQty.Item2) return;
+        CheckCorrectly();   
+    }
+    private void CheckCorrectly()
+    {
+        var isCorrectly = true;
+        for(var i = 0; i < cubeQty.Item1 * cubeQty.Item2; i++)
+            if (!grid.transform.GetChild(i).name.Contains(CorrectlyString))
+                isCorrectly = false;
+        if(isCorrectly)
+            Debug.Log("You win!");
     }
 }
