fd5a2a026cf87f2528fd8fa1dc65d3999e9168dc
Anon4ig_Plovecsky <86836359+Anon4ig-Plovecsky@users.noreply.github.com>
2024-05-24 00:06:58 +0600

    Implemented level teleportation

--- Assets/Scripts/CommonScripts/InputController.cs
+++ Assets/Scripts/CommonScripts/InputController.cs
@@ -1,44 +1,50 @@
+using Valve.VR.InteractionSystem;
 using Unity.VisualScripting;
 using UI.TestedModules;
 using UnityEngine;
 using Valve.VR;
 using System;
 
-public class InputController : MonoBehaviour
+namespace CommonScripts
 {
-    private bool _isError;
-    
-    [SerializeField] private GameObject pauseController;
-    private bool _isPaused;
-
-    [SerializeField] private SteamVR_Action_Boolean actionBoolean;
-    private const SteamVR_Input_Sources InputSource = SteamVR_Input_Sources.LeftHand;
-
-    private GameObject _objPlayer;
-
-    private void Start()
+    public class InputController : MonoBehaviour
     {
-        _objPlayer = GameObject.Find("Player");
-    }
+        private bool _isError;
     
-    private void Update()
-    {
-        try
+        [SerializeField] private GameObject pauseController;
+        private bool _isPaused;
+
+        [SerializeField] private SteamVR_Action_Boolean actionBoolean;
+        private const SteamVR_Input_Sources InputSource = SteamVR_Input_Sources.LeftHand;
+
+        private GameObject _objPlayer;
+
+        private void Start()
         {
-            if (Input.GetKeyDown(KeyCode.Escape) ||
-                !_objPlayer.IsUnityNull() && actionBoolean.GetStateDown(InputSource)
-                && pauseController.activeSelf)
-            {
-                _isPaused = Time.timeScale != 0;
-                CanvasController.ClassCanvasController.SetPause(_isPaused);
-            }
+            Teleport.instance.CancelTeleportHint();
+        
+            _objPlayer = GameObject.Find("Player");
         }
-        catch (NullReferenceException e)
+    
+        private void Update()
         {
-            if (_isError)
-                return;
-            _isError = true;
-            Debug.LogError(e.Message);
+            try
+            {
+                if (Input.GetKeyDown(KeyCode.Escape) ||
+                    !_objPlayer.IsUnityNull() && actionBoolean.GetStateDown(InputSource)
+                                              && pauseController.activeSelf)
+                {
+                    _isPaused = Time.timeScale != 0;
+                    CanvasController.ClassCanvasController.SetPause(_isPaused);
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                if (_isError)
+                    return;
+                _isError = true;
+                Debug.LogError(e.Message);
+            }
         }
     }
 }
